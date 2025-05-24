1805ec5de2dc35e9faab39a82b837da3ff506a87
ljxiang222 <lrxiang222@163.com>
2025-05-24 13:59:04 +0800

    修改接口内容

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,38 +1,44 @@
 import { useRef } from "react";
 import Message from "./Message";
 
-
 type Message = {
-  text: string;
-  sender: "user" | "bot";
+    text: string;
+    sender: "user" | "bot";
 };
 
 type ChatWindowProps = {
-  messages: Message[];
+    messages: Message[];
 };
 
 export default function ChatWindow({ messages }: ChatWindowProps) {
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const scrollBottomBehavior = () => {
-    // messagesEndRef.current?.scrollIntoView({ behavior }); 
-    scrollToBottom('smooth');
-  }
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const scrollBottomBehavior = () => {
+        // messagesEndRef.current?.scrollIntoView({ behavior });
+        scrollToBottom("smooth");
+    };
 
-  // 消息变化时触发滚动
-  const scrollToBottom = (behavior: ScrollBehavior = 'auto') => {
-    messagesEndRef.current?.scrollIntoView({ behavior });
-  }
+    // 消息变化时触发滚动
+    const scrollToBottom = (behavior: ScrollBehavior = "auto") => {
+        messagesEndRef.current?.scrollIntoView({ behavior });
+    };
 
-  return (
-    <div className="pb-[15vh] h-[100vh] overflow-hidden mb-4 pt-[60px] " >
-      <div className="absolute left-0 h-[60px] text-center w-full top-0 flex items-center justify-center"><strong>会话信息</strong></div>
-      <div className="w-full h-full overflow-y-auto" ref={containerRef}>
-        {messages.map((msg, idx) => (
-          <Message key={idx} text={msg.text} sender={msg.sender} scrollBottom={scrollBottomBehavior} />
-        ))}
-        <div ref={messagesEndRef}></div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="pb-[20vh] h-[100vh] overflow-hidden mb-4 pt-[60px] ">
+            <div className="absolute left-0 h-[60px] text-center w-full top-0 flex items-center justify-center">
+                <strong>会话信息</strong>
+            </div>
+            <div className="w-full h-full overflow-y-auto" ref={containerRef}>
+                {messages.map((msg, idx) => (
+                    <Message
+                        key={idx}
+                        text={msg.text}
+                        sender={msg.sender}
+                        scrollBottom={scrollBottomBehavior}
+                    />
+                ))}
+                <div ref={messagesEndRef}></div>
+            </div>
+        </div>
+    );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [tailwindcss()],
     server: {
-        host: "192.168.10.70",
+        host: "192.168.20.70",
         proxy: {
             "/api": {
-                target: "http://192.168.10.9:11434", // URL твоего API
+                target: "http://192.168.20.40:9100", // URL твоего API
                 rewrite: (path) => {
                     return path.replace(/^\/api/, "/api");
                 },
